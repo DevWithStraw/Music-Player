de842685866393d0866f63ba9036409bff39bb0c
Mehdi <01skull01@gmail.com>
2024-11-05 19:22:32 +0330

    added like feature

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -8,6 +8,7 @@ export default function Music({imageSrc ,songName , artist , timeline}) {
           <section>
             <h2 className='poppins-semibold'> {songName} </h2>
             <h3 className='poppins-regular'>  {artist} </h3>
+            <span> Like </span>
           </section>
         <span className='poppins-regular'>{timeline}</span>
     </section>
